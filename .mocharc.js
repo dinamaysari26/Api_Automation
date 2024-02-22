23088a4f6d68c87b58ae45f1ea201a955aab7ee1
dinamaysari26 <dinamayangsari26@gmail.com>
2024-02-22 09:32:45 +0700

    report

--- .mocharc.js
+++ .mocharc.js
@@ -3,5 +3,13 @@
 module.exports = {
     spec: "testCase/*Case.js",
     timeout: 5000,
-    reporter: "spec",
+    reporter: "mochawesome",
+    'reporter-option': [
+    'reportDir=Report', // Report directory
+    'reportFilename=[status]_[datetime]-[name]-report', //Report file name
+    'html=true', // enable html report
+    'json=false', // disable json report
+    'overwrite=false', // disable report file overwrite
+    'timestamp=longDate', // add timestamp to report file name
+    ], //
 };
---
+++ Readme.md
@@ -0,0 +1,12 @@
+<h4>How to run this automation</h4>
+  
+  <ol>
+    <li>install dependencies using npm install</li>
+    <li>run command for run API Automation npm run test:mocha</li>
+    </ol>
+<h4>Mochawesome Report</h4>
+<li>Generated report shall be restore under folder report</li>
+<li>Open report that has name pass_[date]-mochawesome-report.html</li>
+  
+
+![reportTestMocha](https://github.com/dinamaysari26/Api_Automation/assets/159514704/0e9acf67-a551-4e70-b985-3e5ed7897a09)
